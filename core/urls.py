697f589dc2baf6a3a59c895faeb4241e26693a51
Gerlin Gonzalez <sadonfa@gmail.com>
2025-03-10 21:25:14 -0500

    Page of contact

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('inicio/', views.home, name="home"),
+    path('contacto/', views.contact, name="contact"),
+    path('enviar/', views.sendform, name="sendform"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from datetime import datetime, timedelta
 from .models import Cars
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.template.loader import render_to_string
+from coronavip.settings import EMAIL_HOST_USER
 
 # Create your views here.
 
@@ -16,3 +19,38 @@ def home(request):
         'fecha': tomorrow,
         'cars': cars,
     })
+
+def contact(request):
+   
+
+    return render(request, "contact.html", {
+        'title': 'Contactos'
+    })
+
+def sendform(request):
+    if request.method == 'POST':
+        fullname = request.POST['fullname']
+        mail = request.POST['mail']
+        info = request.POST['info']
+
+    subject = 'Comentario de ' + fullname 
+    template = render_to_string(
+            'formulario.html',
+            context={                
+                'name': fullname,      
+                'correo': mail,
+                'info': info,               
+
+                },
+            )        
+
+    message = EmailMultiAlternatives(
+            subject, #Titulo
+            "This is an important message.",
+            EMAIL_HOST_USER, #Remitente
+            ["coronaserviciosvip@gmail.com", mail]) #Destinatario
+
+    message.attach_alternative(template, 'text/html')
+    message.send()  
+    
+    return render(request, "home.html")
